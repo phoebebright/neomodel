48ee1134144f4eadc4fe3671971c3bf9933a9b01
Phoebe B <phoebebright310@gmail.com>
2016-02-29 15:04:48 +0000

    SIGNAL_SUPPORT now disables signals altogether

--- neomodel/signals.py
+++ neomodel/signals.py
@@ -11,18 +11,27 @@ except ImportError:
 
 
 def exec_hook(hook_name, self, *args, **kwargs):
-    if hasattr(self, hook_name):
-        getattr(self, hook_name)(*args, **kwargs)
-    if signals and hasattr(signals, hook_name):
-        sig = getattr(signals, hook_name)
-        sig.send(sender=self.__class__, instance=self)
+    if SIGNAL_SUPPORT:
+        if hasattr(self, hook_name):
+            getattr(self, hook_name)(*args, **kwargs)
+        if signals and hasattr(signals, hook_name):
+            sig = getattr(signals, hook_name)
+            sig.send(sender=self.__class__, instance=self)
 
 
 def hooks(fn):
+
     def hooked(self, *args, **kwargs):
         fn_name = fn.func_name if hasattr(fn, 'func_name') else fn.__name__
-        exec_hook('pre_' + fn_name, self, *args, **kwargs)
+
+        if SIGNAL_SUPPORT:
+            exec_hook('pre_' + fn_name, self, *args, **kwargs)
+
         val = fn(self, *args, **kwargs)
-        exec_hook('post_' + fn_name, self, *args, **kwargs)
+
+        if SIGNAL_SUPPORT:
+            exec_hook('post_' + fn_name, self, *args, **kwargs)
+
         return val
+
     return hooked
